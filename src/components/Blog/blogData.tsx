0b388de7e17760e73ad5104cfe70077afdf3d817
dayattt111 <tumbaldaya@gmail.com>
2025-11-28 20:48:55 +0800

    fix: update blogData structure to match SingleBlog component interface

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -1,47 +1,42 @@
-import { Blog } from "@/types/blog";
-
-const blogData: Blog[] = [
+const blogData = [
   {
     id: 1,
     title: "Best UI components for modern websites",
-    paragraph:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
     image: "/images/blog/blog-01.jpg",
-    author: {
-      name: "Samuyl Joshi",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
+    author: "Samuyl Joshi",
+    slug: "best-ui-components",
     tags: ["creative"],
-    publishDate: "2025",
+    createdAt: "2025-01-15",
+    category: "Design",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor."
   },
   {
     id: 2,
     title: "9 simple ways to improve your design skills",
-    paragraph:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
     image: "/images/blog/blog-02.jpg",
-    author: {
-      name: "Musharof Chy",
-      image: "/images/blog/author-02.png",
-      designation: "Content Writer",
-    },
+    author: "Musharof Chy",
+    slug: "9-ways-improve-design-skills",
     tags: ["computer"],
-    publishDate: "2025",
+    createdAt: "2025-01-14",
+    category: "Design",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor."
   },
   {
     id: 3,
     title: "Tips to quickly improve your coding speed.",
-    paragraph:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
     image: "/images/blog/blog-03.jpg",
-    author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
+    author: "Lethium Deo",
+    slug: "tips-improve-coding-speed",
     tags: ["design"],
-    publishDate: "2025",
+    createdAt: "2025-01-13",
+    category: "Development",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor."
   },
 ];
 export default blogData;
